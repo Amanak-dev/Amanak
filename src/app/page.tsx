f80548abf0e594ac88f2255df23c6f5bae217c17
abanoub-abdelmessih <abanoubabdelmessih110@gmail.com>
2026-01-11 20:20:15 +0200

    Add Status section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import Hero from "@/components/shared/Home/Hero";
+import Status from "@/components/shared/Home/Status";
 
 export default function Home() {
   return (
     <>
       <Hero />
+
+      {/* Main Content */}
+      <main className="py-10">
+        <Status />
+      </main>
     </>
   );
 }
---
+++ src/components/shared/Home/Status.tsx
@@ -0,0 +1,46 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { CircleCheck, FileCheck, Users } from "lucide-react";
+
+const stats = [
+  {
+    icon: FileCheck,
+    value: "98%",
+    label: "بلاغ تم حله",
+  },
+  {
+    icon: Users,
+    value: "10,000+",
+    label: "متطوع نشط",
+  },
+  {
+    icon: CircleCheck,
+    value: "500+",
+    label: "حالة عثور ناجحة",
+  },
+];
+
+export default function Status() {
+  return (
+    <section className="grid grid-cols-3 gap-8">
+      {stats.map((stat, index) => {
+        const Icon = stat.icon;
+
+        return (
+          <Card key={index} className="max-w-sm bg-secondary/50">
+            <CardContent className="flex flex-col items-center justify-center gap-3">
+              <span className="bg-primary p-3 text-white rounded-full">
+                <Icon />
+              </span>
+
+              <span className="font-extrabold text-4xl">{stat.value}</span>
+
+              <p className="text-muted-foreground text-lg font-semibold">
+                {stat.label}
+              </p>
+            </CardContent>
+          </Card>
+        );
+      })}
+    </section>
+  );
+}

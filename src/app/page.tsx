e03ce5cab3ccc0b6ef020f0f96ffcc3d7be3c060
abanoub-abdelmessih <abanoubabdelmessih110@gmail.com>
2026-01-12 20:16:01 +0200

    Add Testimonials Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/shared/Home/Hero";
 import Services from "@/components/shared/Home/Services";
 import Status from "@/components/shared/Home/Status";
+import Testimonials from "@/components/shared/Home/Testimonials";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <main className="py-10">
         <Status />
         <Services />
+        <Testimonials />
       </main>
     </>
   );
